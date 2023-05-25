9b4212fa0116769650ffd08503da7ac8602d6163
nhtan <hoangtan250997@gmail.com>
2023-05-25 18:49:41 +0700

    1st commit

---
+++ src/test/java/com/personalproject/jarsmanagement/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.jarsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
